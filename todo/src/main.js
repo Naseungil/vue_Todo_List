cb161664038203ca4f5abeb449e58df36435a99f
Naseungil <sktmddlf505@naver.com>
2024-11-26 15:24:51 +0900

    bootstrap npm

--- todo/src/main.js
+++ todo/src/main.js
@@ -1,11 +1,15 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+// Add the necessary CSS
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue-next/dist/bootstrap-vue-next.css";
 
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(router);
+
+app.mount("#app");
